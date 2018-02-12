18af6b12354b2ec250726d6de83779ad561f12a7
Markus Müller <markus.mueller.1.g@googlemail.com>
2018-02-13 00:22:33 +0100

    had to add the data files explicitly to the MANIFEST.in to include them automatically in src distribution

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(name='bgc_md',
         author_email='markus.mueller.1.g@gmail.com',
         url='https://projects.bgc-jena.mpg.de/hg/SOIL-R/Code/packageTests/bgc_md',
         packages=find_packages(), #find all packages (multifile modules) recursively
-        py_modules=['external_module'], # external_module.py is a module living outside this dir as an example how to iclude something not 
+        #py_modules=['external_module'], # external_module.py is a module living outside this dir as an example how to iclude something not 
         classifiers = [
         "Programming Language :: Python :: 3",
         "Development Status :: 4 - Beta",
@@ -38,44 +38,17 @@ setup(name='bgc_md',
                 ,'generate_miniaml_model_reports= bgc_md.reports:generate_miniaml_model_reports' # ...
                 ]
         },
-        #dependency_links=[
-        #    'git+https://git@github.com/MPIBGC-TEE/LAPM.git#egg=LAPM',
-        #    'git+https://git@github.com/MPIBGC-TEE/CompartmentalSystems.git#egg=CompartmentalSystems',
-        #    'git+https://git@github.com/mamueller/testinfrastructure.git#egg=testinfrastructure', 
-        #],   
         install_requires=[
             "bibtexparser"
-           # ,"CacheControl"
             ,"CompartmentalSystems"
             ,"testinfrastructure"
             ,"concurrencytest"
-           # ,"future == 0.14.3"
-           # ,"arrow == 0.5.0"
-           # ,"memoized_property == 1.0.2"
-           # ,"##for mendeley"
-           # ,"requests == 2.5.1"
             ,"mendeley "
-           # ,"chardet"
-           # ,"ipython"
-           # ,"matplotlib >= 1.5.3"
-           # ,"ndg-httpsclient"
-           # ,"Pygments"
-           # ,"python-subunit"
             ,"PyYAML"
-           # ,"tqdm"
-           # ,"numpy >=1.11.2"
-           # ,"scipy >=0.17.0"
-           # ,"# mpi4py >=2.0.0"
-           # ,"linecache2"
-           # ,"pyasn1"
-           # ,"# pathos"
-           # ,"plotly"
-           # ,"urllib3"
-           # ,"certifi"
-           # ,"idna"
         ],
         include_package_data=True,
-        zip_safe=False)
+        #zip_safe=False
+     )
 
  
  
