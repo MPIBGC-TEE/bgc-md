86a0edc7bcee501945e4a0de5e93b12c9dcabe77
Markus Müller <markus.mueller.1.g@googlemail.com>
2018-02-21 21:58:17 +0100

    added a view of the model database to the simple webabb

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,9 +4,19 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 from django.shortcuts import render,get_object_or_404
 from .models import Choice,Question
 
+class IndexView(generic.ListView):
+    #template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+        """Return the last five puplished questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+def model_table(request):
+   return HttpResponse('Test') 
 
 def index(request):
 	latest_question_list=Question.objects.order_by('-pub_date')[:5]
