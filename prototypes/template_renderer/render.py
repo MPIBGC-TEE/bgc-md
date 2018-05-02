10a2a935600b66aecdfa85c3bb54e352faaabace
Markus Müller <markus.mueller.1.g@googlemail.com>
2018-05-02 17:28:23 +0200

    changed the renderer to swallow nested templates

--- prototypes/template_renderer/render.py
+++ prototypes/template_renderer/render.py
@@ -1,12 +1,15 @@
 from pathlib import Path
 from bgc_md.ReportInfraStructure import ReportElementList, Header, Math, Meta, Text, Citation, Table, TableRow, Newline, MatplotlibFigure
-fn="template.py"
-p=Path(fn)
 
-with p.open() as f:
-    code=f.read()
-data=5
-exec(code)    
-# call the function
-rel= template(data)
-print(rel.pandoc_markdown())
+
+def render(p,data):
+    with p.open() as f:
+        code=f.read()
+    exec(code,globals(),locals())    
+    # call the function
+    func=locals()['template']
+    rel= func.__call__(data)
+    return rel
+
+print(render(Path("template.py"),5).pandoc_markdown())
+print(render(Path("nestedTemplate.py"),6).pandoc_markdown())
