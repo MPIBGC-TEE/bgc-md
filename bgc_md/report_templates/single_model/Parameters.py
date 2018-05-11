18b4dfaa6e39a7be2a4c12cea2936cb5c6e50a4a
Markus Müller <markus.mueller.1.g@googlemail.com>
2018-05-11 11:54:51 +0200

    finished one version of a table template, started work on the parameter set table

---
+++ bgc_md/report_templates/single_model/Parameters.py
@@ -0,0 +1,21 @@
+def template(model):
+    ps=model.parameter_sets
+    #df=model.section_pandas_df("")
+
+    #header_row = TableRow(
+    #    [Text("Model")
+    #     ,Text("Description")
+    #     ,Text("Unit")
+    #    ]
+    #)
+    #table_format = list("lllcl")
+    #T = Table("State Variables", header_row, table_format)
+    #for i in range(df.ndim):
+    #    df_line=df.loc[i]
+    #    T.add_row(TableRow([
+    #         Text(df_line["name"])
+    #        ,Text(df_line["desc"])
+    #        ,Math(df_line["unit"])
+    #    ]))
+    #rel=T
+    #return rel
--- bgc_md/report_templates/single_model/StateVariables.py
+++ bgc_md/report_templates/single_model/StateVariables.py
@@ -1,43 +1,20 @@
 def template(model):
     df=model.section_pandas_df("state_variables")
-    rel=ReportElementList()
 
-
-        #elif section_name == 'additional_variables':
-        #    rel += Header(model.section_titles[section_name], 1)
-        #    rel += Text("The following table contains the available information regarding this section:")
-        #    rel += model.additional_variables_Table()
-        #elif section_name == 'allocation_coefficients':
-        #    rel += Header(model.section_titles[section_name], 1)
-        #    rel += Text("The following table contains the available information regarding this section:")
-        #    rel += model.allocation_coefficients_Table()
-        #elif section_name == 'components':
-        #    rel += Header(model.section_titles[section_name], 1)
-        #    rel += Text("The following table contains the available information regarding this section:")
-        #    rel += model.components_Table()
-        #elif section_name == 'parameter_sets':
-        #    # parameter_sets are treated completely differently
-        #    pass
-        #else:
-        #    # custom section to be included in the report
-        #    rel += Header(model.section_titles[section_name], 1)
-        #    rel += Text("The following table contains the available information regarding this section:")
-        #    rel += model.variables_Table_from_section(section_name, parameter_values = True)
-    
+    header_row = TableRow(
+        [Text("Model")
+         ,Text("Description")
+         ,Text("Unit")
+        ]
+    )
+    table_format = list("lllcl")
+    T = Table("State Variables", header_row, table_format)
+    for i in range(df.ndim):
+        df_line=df.loc[i]
+        T.add_row(TableRow([
+             Text(df_line["name"])
+            ,Text(df_line["desc"])
+            ,Math(df_line["unit"])
+        ]))
+    rel=T
     return rel
-    
-#def get_all_colnames(complete_dict, variables_sections):
-#    colnames = set()
-#    for sec in variables_sections:
-#        section_dic = section_subdict(complete_dict, sec) # {'state_variables': [...]}
-#        return get_all_colnames_of_section_dict(section_dic)
-#        for sec_name, var_list in section_dic.items():
-#            for var_dic in var_list:
-#                # var_dic = {'C': {'exprs': 'C=...', 'desc': '...'}}
-#                # or var_dic = 'C'
-#                if type(var_dic) == builtins.dict:
-#                    for var, props in var_dic.items():
-#                        if props: # maybe var_dic = {'C': }
-#                            for colname, value in props.items(): # ('desc', '...')
-#                                colnames |= {colname}
-#    return sorted(list(colnames))
