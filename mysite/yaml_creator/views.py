dea2f9048e1b393499565fa6bb655419113a493c
Markus Müller <markus.mueller.1.g@googlemail.com>
2018-06-04 19:46:57 +0200

    extended the pseude form a bit, so that we can now reap the inserted values

--- mysite/yaml_creator/views.py
+++ mysite/yaml_creator/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 from pathlib import Path
 from bgc_md.reports import defaults
+from bgc_md.Model import Model
 # Create your views here.
 def index(request):
     ap=defaults()['paths']['data'].joinpath('all_records')
@@ -19,8 +20,15 @@ def results(request,question_id):
     return HttpResponse("You are looking at the results of question %s." % question_id)
 
 def change(request,file_name):
+    #m=Model.from_
     template=loader.get_template('yaml_creator/change.html')
-    
-    context={'yaml_file_name':file_name,'choices':["x=Bx+s","x=Ax+ub"]}
+    ap=defaults()['paths']['data'].joinpath('all_records')
+    choices=["x=Bx+s","x=Ax+ub"] 
+    #selected_choice=choices[request.POST['choice']-1]
+    print("##############################")
+    print(file_name)
+    print(choices[int(request.POST['choice'])-1])
+    print("##############################")
+    context={'yaml_file_name':file_name,'choices':choices}
     return HttpResponse(template.render(context,request))
 
