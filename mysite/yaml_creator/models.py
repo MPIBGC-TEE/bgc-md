ad1a0d940ed3fbc66f8587e036d19389525afe59
Markus Müller <markus.mueller.1.g@googlemail.com>
2018-06-11 12:41:50 +0200

    started a relational representations of the models

--- mysite/yaml_creator/models.py
+++ mysite/yaml_creator/models.py
@@ -3,5 +3,12 @@ from django.utils import timezone
 
 # Create your models here.
 
+class ModelDescriptor(models.Model):
+    name=models.CharField(max_length=200)
+    doi=models.CharField(max_length=200)
+    pub_date=models.DateTimeField('date published')
 
-
+class Variable(models.Model):
+    name=models.CharField(max_length=200)
+    model_descriptor=models.ForeignKey(ModelDescriptor,on_delete=models.CASCADE)
+    reverse_execution_order_position=models.IntegerField(default=0)
