c7c44f5abb56f2309912ec6b293908abfa638787
Markus Müller <markus.mueller.1.g@googlemail.com>
2018-06-19 12:46:06 +0200

    figured out the oneToMany relationship of Variables and SingleFluxes in the test.py prototype

--- mysite/test.py
+++ mysite/test.py
@@ -3,8 +3,16 @@ from yaml_creator.models.SingleFlux import SingleFlux
 from yaml_creator.models.Variable import Variable
 md=ModelDescriptor.objects.get(pk='default_1.yaml')
 a=Variable.objects.create(name='a',model_descriptor=md)
+a.save()
 b=Variable.objects.create(name='b',model_descriptor=md)
+b.save()
 c=Variable.objects.create(name='c',model_descriptor=md)
+c.save()
 ab=SingleFlux(source=a,target=b)
+ab.save()
 ac=SingleFlux(source=a,target=c)
+ac.save()
 bc=SingleFlux(source=b,target=c)
+bc.save()
+print(a.donates.all())
+
