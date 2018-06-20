5f5b65efbdc4c058d12c294b2a3852db9d52a1c4
Markus Müller <markus.mueller.1.g@googlemail.com>
2018-06-20 19:32:49 +0200

    secured the Variable class against duplication of a Variable name per Model

--- prototypes/testSingleFlux.py
+++ prototypes/testSingleFlux.py
@@ -1,4 +1,4 @@
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist,ValidationError
 from django.utils import timezone
 from yaml_creator.models.ModelDescriptor import ModelDescriptor
 from yaml_creator.models.SingleFlux import SingleFlux
@@ -13,16 +13,25 @@ for var in Variable.objects.all():
     var.delete()
 print(md)
 a=Variable.objects.create(name='a',model_descriptor=md)
-#a.save()
-#b=Variable.objects.create(name='b',model_descriptor=md)
-#b.save()
-#c=Variable.objects.create(name='c',model_descriptor=md)
-#c.save()
-#ab=SingleFlux(source=a,target=b)
-#ab.save()
-#ac=SingleFlux(source=a,target=c)
-#ac.save()
-#bc=SingleFlux(source=b,target=c)
-#bc.save()
-#print(a.donating.all())
+
+b=Variable.objects.create(name='b',model_descriptor=md)
+c=Variable.objects.create(name='c',model_descriptor=md)
+ab=SingleFlux(source=a,target=b)
+ab.save()
+ac=SingleFlux(source=a,target=c)
+ac.save()
+bc=SingleFlux(source=b,target=c)
+bc.save()
+print(a.donating.all())
+try:
+    a=Variable.create(name='a',model_descriptor=md)
+except ValidationError as e:
+    print(e)
+
+try:
+    a=Variable(name='a',model_descriptor=md)
+    a.save()
+except ValidationError as e:
+    print(e)
+
 
