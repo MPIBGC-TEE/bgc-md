931a6e37eb9db0248cf0d54b59bffae5a41581d8
Markus Müller <markus.mueller.1.g@googlemail.com>
2018-09-07 18:55:31 +0200

    played around with a template

--- bgc_md/report_templates/single_model/TransientSystemAgeDensity3d.py
+++ bgc_md/report_templates/single_model/TransientSystemAgeDensity3d.py
@@ -19,13 +19,15 @@ def template(model):
         lmr = mr.linearize()
         
         start_age_densities=start_age_distributions_from_zero_age_initial_content(srm,mr.start_values)
-        #lmr.times = np.linspace(0,2,3)
+        lmr.times = np.linspace(0,5,101)
         nr_age_steps = 20
         age_stride = 1
-        time_stride = 10
+        #time_stride = 10
+        time_stride = 1
 
-        ages = np.linspace(lmr.times[0], lmr.times[-1], nr_age_steps)
-        #ages = lmr.times
+        #ages = np.linspace(lmr.times[0], lmr.times[-1], nr_age_steps)
+        ages = lmr.times
+        ages=np.linspace(0,0.1,101)
         p = lmr.pool_age_densities_func(start_age_densities)
         pool_age_densities = p(ages)
         system_age_density = lmr.system_age_density(pool_age_densities)
@@ -33,8 +35,8 @@ def template(model):
         title = 'System age density evolution'
         fig = lmr.plot_3d_density_plotly(
                     title,
-                    system_age_density,
-                    #pool_age_densities[1],
+                    #system_age_density,
+                    pool_age_densities[1],
                     ages,
                     age_stride=age_stride,
                     time_stride=time_stride
