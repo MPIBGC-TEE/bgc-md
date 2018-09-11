852caa7c2398151e917e093ca3f12c7fe43912d5
mm <markus.mueller.1.g@googlemail.com>
2018-09-12 00:07:36 +0200

    typo corrected

--- prototypes/Metaclasses/LittleMeta.py
+++ prototypes/Metaclasses/LittleMeta.py
@@ -1,7 +1,7 @@
 class LittleMeta(type):
     def __new__(cls,clsname,superclasses,attributedict):
         print("clsname: ",clsname)
-        print("superclasses: ",clsname)
+        print("superclasses: ",superclasses)
         print('attributedict: ',attributedict)
         return type.__new__(cls,clsname,superclasses,attributedict)
 
