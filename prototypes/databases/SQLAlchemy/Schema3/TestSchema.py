886d72f3ced0bb419e294cd18e7514d72de3e8bb
mm <markus.mueller.1.g@googlemail.com>
2019-01-16 12:56:07 +0100

    started to work on the functional interface

--- prototypes/databases/SQLAlchemy/Schema3/TestSchema.py
+++ prototypes/databases/SQLAlchemy/Schema3/TestSchema.py
@@ -5,15 +5,10 @@
 
 
 import unittest
-#import exampleModels
-from sqlalchemy import Table, Column, Integer, String, MetaData,ForeignKey,ForeignKeyConstraint
-from sqlalchemy import create_engine
-from sqlalchemy.sql import select
-#from sympy import Matrix,sympify,symbols,Symbol,IndexedBase
 from testinfrastructure.helpers import pe
-#from createTables import createTables
 #from sympy import Basic,Symbol,Matrix,symbols
-from sympy.vector import CoordSysND, Vector,express
+
+#from sympy.vector import CoordSysND, Vector,express
 from helpers import getModelDescriptor
 from helpers import get
 
@@ -21,38 +16,8 @@ from helpers import get
 class TestSchema(unittest.TestCase):
     # The aim is a proof of concept implementation for the retrieval of the information that is neccessary to build the 
     # compartmental Matrix
-    # Conceptually we want to separate this information from the database, which should only hold
-    # the Variables and the statevectorpositions which together already determine the matrices
-
-    #def setUp(self):
-    #    engine = create_engine('sqlite:///:memory:')
-    #    metadata = MetaData()
-    #    
-    #    # build the tables
-    #    Models=Table(
-    #        'Models'
-    #        ,metadata
-    #    	,Column('id', String, primary_key=True)
-    #    	,Column('name', String)
-    #    )
-    #    metadata.create_all(engine)
-
-    #    self.metadata=metadata
-    #    self.engine=engine
-
+    
     def test_CS_creation(self):
-        #metadata = self.metadata
-        #engine   = self.engine
-        #Models=Table("Models",metadata,autoload=True,autoload_with=engine)
-        #model_id="testFivePool"
-        #model_name="test"
-        #conn=engine.connect()
-        #conn.execute(
-        #    Models.insert(),
-        #    [
-        #        {'id':model_id,'name':model_name},
-        #    ]
-        #)
         md=get(model_id='testFivePool',callString='get_ModelDescriptor()')
         pe('md.compartmental_matrix',locals())
 
@@ -72,8 +37,19 @@ class TestSchema(unittest.TestCase):
         # now we define a category distribution vector
         #b_five=VegDistVector(leaf=Ivl/u_org,
         print(
-            get(model_id='testFivePool',callString='get_cumulative_Vegetation_Input()')
+            get(model_id='testTwoPool',callString='get_cumulative_Vegetation_Input()')
             ,get(model_id='testFivePool',callString='get_cumulative_Vegetation_Input()')
         )
+        
+    def test_polymorph(self):
+        # many model properties can be computed from different sources
+        # We will implement the following strategy
+        # 1.) Check if the user provided a function to compute the desired property
+        # 2.) Check if the property  can be computed from other properties provided by the user.
+        # 3.) If there are more then one ways to get the result check for consistency
+         
+        # we demonstrate this by a model that does not any function for get_InputVector but 
+        # only for the separate fluxes
+        get(model_id='testVectorFree',callString='get_InputVector()')
         
 
