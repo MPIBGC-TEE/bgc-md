d5842e762e23f2018f20e892e4a558e809d389e2
Markus Müller <markus.mueller.1.g@googlemail.com>
2019-09-23 15:57:29 +0200

    removed another typo from the spinup script and the duplication that lead to it

--- prototypes/ModelsAsScriptsWithSpecialVarNames/models/miniCable/cable_all/cable_transit_time/example_runs/parallel_200_year/runspinup_mm.sh
+++ prototypes/ModelsAsScriptsWithSpecialVarNames/models/miniCable/cable_all/cable_transit_time/example_runs/parallel_200_year/runspinup_mm.sh
@@ -3,6 +3,8 @@ set -e
 odir="output/new4"
 spinStart=1901
 spinEnd=1910
+spindumptrunk="cnpspindump"
+# 
 runSpinupYears() {
   echo "got value i=$1"
   i_max=$1
@@ -26,52 +28,58 @@ runSpinupYears() {
       mv restart_out.nc    ${odir}/restart_ncar_${i}_${yr}_ndep.nc
       mv poolcnp_out.csv   ${odir}/poolcnp_out_${i}_${yr}_ndep.csv
       if [ $i -eq $i_max];then
-        cp -p cnpspindump${yr}.nc ${odir}/
+        cp -p ${spindumptrunk}${yr}.nc ${odir}/
       fi
       yr=`expr $yr + 1`
     done
     i=`expr $i + 1`
   done
 }
+step1(){
+  cd ../spinup
+  ./mknml_mm.bash
+  
+  echo "######################################################################################################"
+  echo copy pool file
+  cp -p ../../CABLE-run-test-lqy/spinup/poolcnp_in.csv ./poolcnp_in.csv
+  
+  echo "######################################################################################################"
+  echo copy restart file
+  cp -p  ../../CABLE-run-test-lqy/spinup/restart_in.nc ./restart_in.nc
+  
+  
+  #prepare directory
+  mkdir -p ${odir}
+  
+  runSpinupYears 5
+  echo "######################################################################################################"
+  echo "after first loop"
 
-cd ../spinup
-./mknml_mm.bash
+}
 
-echo "######################################################################################################"
-echo copy pool file
-cp -p ../../CABLE-run-test-lqy/spinup/poolcnp_in.csv ./poolcnp_in.csv
+step2(){
+  lstFileName="fcnpspin.lst"
+  echo 10 > $lstFileName
+  for i in $(seq $spinStart ${spinEnd})
+  do  
+    echo "${odir}/${spindumptrunk}${i}.nc" >> $lstFileName
+  done
+  cp -p cable_CN_spincasa_${spinEnd}.nml cable.nml
+  mpirun -np 9 --oversubscribe ../../CABLE-SRC/offline/tmpParallel/cable-mpi
+  mv out_cable.nc     ${odir}/out_ncar_${i}_0_ndep.nc
+  cp -p restart_out.nc restart_in.nc
+  cp -p poolcnp_out.csv poolcnp_in.csv
+  mv restart_out.nc ${odir}/restart_ncar_${spinEnd}_ndep.nc
+  mv poolcnp_out.csv ${odir}/poolcnp_out_${spinEnd}_ndep.csv
+  echo "######################################################################################################"
+  echo "after SASU"
+}
 
-echo "######################################################################################################"
-echo copy restart file
-cp -p  ../../CABLE-run-test-lqy/spinup/restart_in.nc ./restart_in.nc
-
-
-#prepare directory
-mkdir -p ${odir}
-
-runSpinupYears 5
-echo "######################################################################################################"
-echo "after first loop"
-
-
-
-lstFileName="fcnpspin.lst"
-echo 10 > $lstFileName
-for i in $(seq $spinStart ${spinEnd})
-do  
-  echo "${odir}/cnspindump${i}.nc" >> $lstFileName
-done
-cp -p cable_CN_spincasa_${yr}.nml cable.nml
-mpirun -np 9 --oversubscribe ../../CABLE-SRC/offline/tmpParallel/cable-mpi
-mv out_cable.nc     ${odir}/out_ncar_${i}_0_ndep.nc
-cp -p restart_out.nc restart_in.nc
-cp -p poolcnp_out.csv poolcnp_in.csv
-mv restart_out.nc ${odir}/restart_ncar_${yr}_ndep.nc
-mv poolcnp_out.csv ${odir}/poolcnp_out_${yr}_ndep.csv
-echo "######################################################################################################"
-echo "after SASU"
-
-
-runSpinupYears 10
-echo "######################################################################################################"
-echo "after second loop"
+step3(){
+  runSpinupYears 10
+  echo "######################################################################################################"
+  echo "after second loop"
+}
+step1
+step2
+step3
